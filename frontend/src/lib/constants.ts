1483b88e02a2150dfc1daf80112ef5611b616b40
jenisha <jenisha0413@gmail.com>
2026-07-24 21:21:55 +0545

    Deploy to devnet and update frontend

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const PROGRAM_ID = "2h198Q9MNSFGWo2aJeEq2CRFSSfykfFVKUkbDJhyzrAf";
 
-export const NETWORK = "localnet";
+export const NETWORK = "devnet";
 
 export const COMMITMENT = "confirmed";
 
@@ -10,4 +10,4 @@ export const VOTE_SEED_PREFIX = "vote";
 
 export const EXPLORER_BASE_URL = "https://explorer.solana.com";
 
-export const RPC_ENDPOINT = "http://localhost:8899";
+export const RPC_ENDPOINT = "https://api.devnet.solana.com";
